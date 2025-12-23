8edb84cf25e679ff93635b75c27a2477b665ef6c
Brian Augusto Perez <44121002+brianaperez@users.noreply.github.com>
2025-12-23 16:21:45 -0500

    modified setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from glob       import glob
 import os
 
 package_name = 'perception_ros'
-
+csv_file = 'placeholder.csv'
 
 setup(
     name=package_name,
@@ -13,6 +13,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['share/' + package_name + '/' + csv_file]),
+        ('share/' + package_name + '/launch', ['launch/perception_ros_launch.py']),
+        ('share/' + package_name + '/Perception', glob('Perception/*.pt'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +27,13 @@ setup(
     entry_points={
         'console_scripts': [
         'perception = perception_ros.perception:main',
+        'controller = perception_ros.controller:main',
+        'position = perception_ros.position:main',
+        'track = perception_ros.track:main',
+        'error_recorder = perception_ros.error:main',
+        'steering_recorder = perception_ros.steer:main',
+        'velocity_recorder = perception_ros.velocity:main'
+        # 'live_map = perception_ros.live_map:main' 
         ],
     },
 )
